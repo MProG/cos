545e79109bf67d402dc54b254dff8f3b8d4a2e58
Shutkov Maksim <shutkov.maksim@mail.ru>
2017-10-05 14:33:36 +0300

    added second integrall

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,7 +2,6 @@ class GraphsController < ApplicationController
   before_action :find_graph, only: [:show, :destroy]
 
   def index
-    binding.pry
     @graphs = Graph.all
   end
 
@@ -13,13 +12,18 @@ class GraphsController < ApplicationController
 
   def show
     @graph_data = GraphBuilderService.new.build(@graph)
-    @integral_graph = IntegralBuilderService.new.build(@graph)
+    @integral_graph = integral_service.build
+    @second_integral_graph = integral_service.second_build
   end
 
   def new
   end
 
   private
+
+  def integral_service
+    @_integral_service ||= IntegralBuilderService.new(@graph)
+  end
 
   def find_graph
     @graph = Graph.find(params[:id])
--- app/services/integral_builder_service.rb
+++ app/services/integral_builder_service.rb
@@ -1,29 +1,43 @@
 class IntegralBuilderService
-  attr_reader :graph
+  attr_reader :graph, :first_values
 
-  def build(graph)
-    data = graph_data(graph)
-    GraphCreateService.new.write_graph(data[:data], graph.name, math_values(data[:values]))
+  def initialize(graph)
+    @graph = graph
+  end
+
+  def build
+    GraphCreateService.new.write_graph(first_integral[:data], graph.name, math_values(first_integral[:values]))
+  end
+
+  def second_build
+    second_values = graph_data(first_integral[:values])
+    GraphCreateService.new.write_graph(second_values[:data], graph.name, math_values(second_values[:values]))
   end
 
   private
 
-  def graph_data(graph)
-    point_y = 0.0
+  def first_integral
+    @_first_integral ||= graph_data(@graph.values)
+  end
+
+  def graph_data(values)
+    point_x = 0.0
     data = []
-    point_x = 0
+    point_y = 0
     new_values = []
 
-    step = 1.0 / graph.values_in_sec
-
-    graph.values.each do |value|
-      point_x += value * step
-      data << [point_y, point_x]
-      new_values << point_x
-      point_y += step
+    values.each do |value|
+      point_y += value * step
+      data << [point_x, point_y]
+      new_values << point_y
+      point_x += step
     end
 
     { data: data, values: new_values }
+  end
+
+  def step
+    @_step ||= 1.0 / graph.values_in_sec
   end
 
   def math_values(values)
